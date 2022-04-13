68502647cb6f81c59f4e72702bfe71861b6dee51
unknown <Lily@1990>
2022-04-13 09:52:39 +0300

    vuewith tailwind

---
+++ src/components/ButtonCom.vue
@@ -0,0 +1,12 @@
+<template>
+   <button class="p-2 bg-gray-400 m-3">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonCom',
+  props: {
+    text: String
+  }
+}
+</script>
